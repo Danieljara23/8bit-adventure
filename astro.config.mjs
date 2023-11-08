34be8cf569c8ae4cb21cae123421a95582100b28
Daniel Jaramillo <grisales@google.com>
2023-11-08 00:50:22 -0500

    fix: missing comma in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danieljara23.github.io',
-  base: '/8bit-adventure'
+  base: '/8bit-adventure',
   integrations: [react()]
 });
